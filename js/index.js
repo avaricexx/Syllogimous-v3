0b7c82a6e1258dfacada6c09a2ad2a320d56ea9c
avaricexx <0xAvar1ce@proton.me>
2025-03-31 11:31:08 +0200

    Add fullscreen mode

--- js/index.js
+++ js/index.js
@@ -1033,6 +1033,34 @@ function handleKeyPress(event) {
     }
 }
 
+function toggleFullscreen() {
+    if (!document.fullscreenElement &&
+        !document.mozFullScreenElement && // Firefox
+        !document.webkitFullscreenElement && // Chrome, Safari and Opera
+        !document.msFullscreenElement) {  // IE/Edge
+
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) { // Firefox
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+            document.documentElement.webkitRequestFullscreen();
+        } else if (document.documentElement.msRequestFullscreen) { // IE/Edge
+            document.documentElement.msRequestFullscreen();
+        }
+    } else {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) { // Firefox
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { // Chrome, Safari and Opera
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { // IE/Edge
+            document.msExitFullscreen();
+        }
+    }
+}
+
 document.addEventListener("keydown", handleKeyPress);
 
 registerEventHandlers();
